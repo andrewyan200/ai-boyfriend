a5416bbd8c07f822220fa6a9d2b54f31281fecd4
Andrew Yan <andrew.m.yan@gmail.com>
2024-02-18 00:14:51 -0800

    instacart auto-order working

--- instacart.py
+++ instacart.py
@@ -9,8 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
 import time
+import undetected_chromedriver as webdriver
+from capsolver_python import RecaptchaV2Task
 
 load_dotenv()
+CAPTCHA_API_KEY = os.getenv("CAPTCHA_API_KEY")
 INSTACART_EMAIL = os.getenv("INSTACART_EMAIL")
 INSTACART_PASSWORD = os.getenv("INSTACART_PASSWORD")
 COFFEE_LINK = "https://www.instacart.com/products/248557-high-brew-cold-brew-coffee-mexican-vanilla-8-fl-oz?retailerSlug=safeway"
@@ -18,14 +21,10 @@ MEDICINE_LINK = "https://www.instacart.com/products/90236-vicks-dayquil-and-nyqu
 CHECKOUT_LINK = "https://www.instacart.com/store/checkout_v4?sid=53040"
 DELIVERY_ADDRESS = "475 Via Ortega, Stanford, CA 94305"
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--use_subprocess")
 
-
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 # URL of the Instacart CVS storefront
@@ -44,7 +43,15 @@ time.sleep(1)
 submit_button = driver.find_element(
     By.CSS_SELECTOR, "button[class='e-ztomkz']")
 submit_button.click()
-time.sleep(10)
+
+# capsolver = RecaptchaV2Task(CAPTCHA_API_KEY)
+# task_id = capsolver.create_task(login_url, "website_key")
+# result = capsolver.join_task_result(task_id)
+# print(result.get("gRecaptchaResponse"))
+# time.sleep(30)
+
+
+time.sleep(5)
 # CVS_button = driver.find_element(By.CSS_SELECTOR, "img[alt='CVS®']")
 # CVS_button.click()
 # driver.implicitly_wait(5)
@@ -53,12 +60,10 @@ time.sleep(10)
 product_link = "https://www.instacart.com/products/84345-sambucol-cold-flu-relief-black-elderberry-quick-dissolve-tablets-30-ea?retailerSlug=cvs"
 driver.get(product_link)
 
-time.sleep(5)
+time.sleep(12)
 
-add_button = driver.find_element(
-    By.CSS_SELECTOR, "button[class='e-1mchykm']")
-
-add_button.click()
+button = driver.find_element(By.CLASS_NAME, 'e-1mchykm')
+button.click()
 
 driver.implicitly_wait(5)
 driver.get("https://www.instacart.com/store/checkout_v4?sid=53040")
@@ -68,8 +73,10 @@ driver.get("https://www.instacart.com/store/checkout_v4?sid=53040")
 # save_and_continue = driver.find_element(By.CLASS_NAME, "e-15utg5h")
 # save_and_continue.click()
 # time.sleep(8)
-choose_delivery_time = driver.find_element(By.CLASS_NAME, "e-rloafg")
-choose_delivery_time.click()
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.e-rloafg")))
+element.click()
+time.sleep(4)
 # phone_number = driver.find_element(By.CSS_SELECTOR, "input[type='tel']")
 # phone_number.send_keys("2034518641")
 # submit_phone_number = driver.find_element(By.CLASS_NAME, "e-sp84se")
@@ -77,13 +84,20 @@ choose_delivery_time.click()
 # time.sleep(4)
 final_continue = driver.find_element(By.CLASS_NAME, "e-15utg5h")
 final_continue.click()
-time.sleep(8)
+time.sleep(3)
+
+button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Continue')]]"))
+)
+button.click()
+
+time.sleep(10)
 # Find the span element by its text content "Place order"
 place_order_span = driver.find_element(
     By.XPATH, "//span[text()='Place order']")
-
 # Click the span element
 place_order_span.click()
+print("Order placed!!")
 # cough_drop_button = driver.find_element(
 #     By.CSS_SELECTOR, "button[aria-label='Add 1 item Halls Relief Honey Lemon Cough Suppressant/Oral Anesthetic Menthol Drops']")
 # cough_drop_button.click()
